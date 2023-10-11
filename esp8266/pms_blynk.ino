a70fb990f209c7b3f6338cd5995460311b71a37d
Chanawat Thuasuphap <earthza991@hotmail.com>
2023-10-12 05:09:14 +0700

    remove unused code

--- esp8266/pms_blynk.ino
+++ esp8266/pms_blynk.ino
@@ -1,13 +1,10 @@
-
-
 #define BLYNK_TEMPLATE_ID "TMPL6xaRjGL92"
 #define BLYNK_TEMPLATE_NAME "PMS7003Template"
 #define BLYNK_AUTH_TOKEN "ct3blJmW7AhiID9Ig3wc0aM81wZFECOp"
-
 #define BLYNK_PRINT Serial
+
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
-
 #include <SoftwareSerial.h>
 
 #define RX_PIN 5
@@ -15,8 +12,6 @@
 
 SoftwareSerial pms_serial(RX_PIN, TX_PIN);
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
 char ssid[] = "R202 WiFi";
 char pass[] = "!q2w3e4r5t";
 BlynkTimer timer;
@@ -30,20 +25,17 @@ bool delay_mode = false;
 bool blynk_control_all_led = false;
 
 
-// This function is called every time the device is connected to the Blynk.Cloud
 BLYNK_CONNECTED()
 {
-  // Change Web Link Button message to "Congratulations!"
   Blynk.setProperty(V100 ,"offImageUrl", "https://staticimage.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations.png");
   Blynk.setProperty(V100 ,"onImageUrl", "https://staticimage.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
   Blynk.setProperty(V100, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstartdevice-was-made");
 }
 
-// This function sends Arduino's uptime every second to Virtual Pin 2.
 void ReadPMS()
 {
   if (pms_serial.available()) {
-    String data = pms_serial.readStringUntil('\n'); // Read data until newline character
+    String data = pms_serial.readStringUntil('\n'); 
     int pm1_0, pm2_5, pm10_0;
     sscanf(data.c_str(), "%d,%d,%d", &pm1_0, &pm2_5, &pm10_0);
 
